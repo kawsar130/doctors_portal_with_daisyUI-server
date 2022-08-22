190babab46a9209d2b5cf3d0a0232f7e42e15e60
Kawsar Ahmed <kawsarahmed130@gmail.com>
2022-08-23 01:08:52 +0600

    Booking appointment updated with date query

--- index.js
+++ index.js
@@ -22,12 +22,40 @@ async function run() {
     const serviceCollection = client
       .db("doctors_portal")
       .collection("services");
+    const bookingCollection = client
+      .db("doctors_portal")
+      .collection("bookings");
 
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    /*
+     * API Naming conventions
+     * --------------------------------
+     * app.get("/booking") // get all the bookings in this collection. or get more than one or by filter
+     * app.get("/booking/:id") // get a specific booking in this collection
+     * app.post("/booking") // add a new booking/create a new booking
+     * app.patch("/booking/:id") // update a specific booking
+     * app.delete("/booking") // delete a specific booking
+     */
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
     });
   } finally {
   }
